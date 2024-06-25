5bb7e44b75fdea7acd8f3018ba75dd7151dd9a79
wschoi-tri <39798559+wschoi-tri@users.noreply.github.com>
2024-06-25 06:44:02 +0000

    소량재고

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import urllib3
 # 심각 : 356049403
 
 comtype = st.radio(
-    "유사상품 또는 개인화 추천을 선택하세요",
+    "조회 구분",
     ["소량재고","유사상품 추천","개인화 추천"]
 )
 
@@ -19,67 +19,53 @@ else:
 
 prd_no = ""
 if comtype != "소량재고":
-    prd_no = st.text_input("상품 추천 TEST", placeholder=placetext + "번호를 입력하세요")
+    prd_no = st.text_input("상품 추천 TEST", placeholder=placetext + "번호를 입력하세요").strip()
     st.write("입력된 " +placetext+ "번호 : ", prd_no)
-
 
 try:
     http = urllib3.PoolManager()
 
     oriImage = ""
+    url = ""
+    data = {}
 
     if prd_no != "":
-
         if comtype == "유사상품 추천":
             oridata = http.request("GET", "http://apix.halfclub.com/searches/prdList/?keyword=" + prd_no + "&siteCd=1&device=mc").json()
             oriImage = oridata["data"]["result"]["hits"]["hits"][0]["_source"]["appPrdImgUrl"]
             st.image(oriImage)
-            st.markdown("""---""")
+            # st.markdown("""---""")
 
         if comtype == "유사상품 추천":
-            data = http.request("GET", "http://develop-api.halfclub.com/searches/recommProducts/?prdNo=" + prd_no).json()
+            url = "http://develop-api.halfclub.com/searches/recommProducts/?prdNo=" + prd_no
         else:
-            data = http.request("GET", "http://develop-api.halfclub.com/searches/personalProducts/?memNo=" + prd_no).json()
+            url = "http://develop-api.halfclub.com/searches/personalProducts/?memNo=" + prd_no
         
-        recommend_list = data["data"]
-        #st.json(recommend_list)
-        result_container = st.container()
-        link_container = st.container()
-        recognition_result_container = result_container.columns(4)
-
-        i=0
-        for recommend in recommend_list:
-            try:
-                with recognition_result_container[i%4]:
-                    st.image(recommend["appPrdImgUrl"], caption= str(recommend["prdNo"]) + " | "+ str(format(recommend["dcPrcApp"], ',')) + "원")
-                    with st.container():
-                        st.link_button(str(recommend["prdNo"]), str(recommend["appPrdDtlUrl"]))    
-                i=i+1
-            except Exception as ex:
-                st.text(ex)
-                
-        st.markdown("""---""")
-        st.json(recommend_list)
     elif comtype == "소량재고":
-        data = http.request("GET", "https://develop-api.halfclub.com/searches/lowStockProductList/").json()
+        url = "https://develop-api.halfclub.com/searches/lowStockProductList/"
+
+    data = http.request("GET", url).json()
+    st.markdown("""---""")
+    st.text_input("Request API URL", url)
+    st.markdown("""---""")
+
+    if len(data["data"]) > 0:
         recommend_list = data["data"]
         result_container = st.container()
         link_container = st.container()
         recognition_result_container = result_container.columns(4)
-
         i=0
         for recommend in recommend_list:
             try:
                 with recognition_result_container[i%4]:
+                    st.write("상품 이동 : [" + str(recommend["prdNo"]) + "](" + str(recommend["appPrdDtlUrl"]) + ")")
                     st.image(recommend["appPrdImgUrl"], caption= str(recommend["prdNo"]) + " | "+ str(format(recommend["dcPrcApp"], ',')) + "원")
-                    with st.container():
-                        st.link_button(str(recommend["prdNo"]), str(recommend["appPrdDtlUrl"]))    
                 i=i+1
             except Exception as ex:
-                st.text(ex)
-                
+                st.text(ex)                
         st.markdown("""---""")
         st.json(recommend_list)
+    
 
 except Exception as ex:
     st.text(ex)
