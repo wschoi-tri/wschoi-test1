ba78a5b75a0d975a8701a4894ae101b0fcaf8db3
wschoi-tri <39798559+wschoi-tri@users.noreply.github.com>
2025-08-04 15:14:00 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ view_options = [
     {"prd_nm": "셔츠", "prd_no": 380125465, "prd_img": "https://cdn2.halfclub.com/rimg/500x667/contain/cdn/product/SA003626/P353033327/1_P353033327_basic_1736536730865.jpg?format=webp"},
     {"prd_nm": "티셔츠", "prd_no": 397585041, "prd_img": "https://cdn2.halfclub.com/rimg/500x667/contain/cdn/product/SA004976/P397585041/1_P397585041_basic_1749777889548.jpg?format=webp"},
     {"prd_nm": "남성정장", "prd_no": 385756116, "prd_img": "https://cdn2.halfclub.com/rimg/500x667/contain/cdn/product/A6232/P385756116/1_P385756116_basic_1751335941326.jpg?format=webp"},
-    {"prd_nm": "골프웨어", "prd_no": 387057774, "prd_img": "https://cdn2.halfclub.com/rimg/500x667/contain/cdn/product/SA003881/P387057774/1_P387057774_basic_1742521480246.jpg?format=webp"},
+    {"prd_nm": "골프웨어", "prd_no": 360321880, "prd_img": "https://cdn2.halfclub.com/rimg/500x667/contain/cdn/product/A1939/P360321880/1_P360321880_basic_1712804679955.jpg?format=webp"},
     {"prd_nm": "스포츠웨어", "prd_no": 393854538, "prd_img": "https://cdn2.halfclub.com/rimg/500x667/contain/cdn/product/SA004454/P393854538/1_P393854538_basic_1746690189142.jpg?format=webp"},
     {"prd_nm": "아웃도어", "prd_no": 392879248, "prd_img": "https://cdn2.halfclub.com/rimg/500x667/contain/cdn/product/SA004416/P392879248/1_P392879248_basic_1749776418523.jpg?format=webp"},
 ]
